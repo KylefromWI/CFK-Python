7fdc62efc67981aaee1eaf14f9ee96be41de3eb6
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2025-07-14 15:26:42 -0500

    Update and rename 06-01-highScore to 06-01-highScore.py

--- 06-01-highScore.py
+++ 06-01-highScore.py
@@ -1,17 +1,19 @@
 #Example of file manipulation (reading and writing)
 
 # asks the user for their score
-choice = input("Do you want to write or read?")
+choice = input("Do you want to write or read?  ")
 
+#User is writing a new score
 if choice =="write":
     #asks the user for their score
-    score = input("What's your high score now? (I trust you)")
+    score = input("What's your high score now? (I trust you)  ")
     #opens the text file named "scores" and sets the mode to Write
     with open("scores.txt", "w") as file:
         #adds the user's score to the file named scores
         file.write(str(score))
         print("Score saved, thank you")
 
+#User is reading the current high score
 if choice =="read":
     with open("scores.txt") as file:
         contents = file.read() # reads a string from a file
