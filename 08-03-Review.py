807acc8d670099934a53edd697012edf5fd4a2fa
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2025-07-16 14:07:20 -0500

    Create 08-03-Review.py

---
+++ 08-03-Review.py
@@ -0,0 +1,28 @@
+# Programming. How it works. How computers work. 
+# Syntax is the order words need to be used. And the specific style and patterns needed behind the words  
+
+
+# Comments: What are comments? Why use them? How do you use them? Single line and multiple lines
+
+
+# Variables: What are they? How do you use them? 
+
+
+# Data types: What are the types? strings, numbers, list, boolean. How can you tell which type of data? type(variable)
+
+
+# Numbers: what is an int? what is a float?
+
+
+# What to do with numbers? We can do math! School bus example. 619 students, 48 per bus (do this together): 
+
+
+# Your goal: write a line of code that prints the total price of a dozen eggs. Here are two variables for you to use
+price_of_one_egg = 0.50
+dozen = 12
+
+
+# What is input? What does IO mean? How do you use it in Python? 
+
+
+# Your goal: 
